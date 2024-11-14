3778a758fae8c40b9bf815a11dacaea06e607c4d
GUILHERME SOUZA GOMES <guilhermesouza.gomes@a.ucb.br>
2024-11-14 10:56:50 -0300

    DAO

--- Confeitaria/src/br/com/controle/Produto.java
+++ Confeitaria/src/br/com/controle/Produto.java
@@ -9,5 +9,50 @@ package br.com.controle;
  * @author gustavo.santos2023
  */
 public class Produto {
+    private int id;
+    private String nome;
+    private double preco_grama;
+    private double peso;
+    private double preco_final;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getPreco_grama() {
+        return preco_grama;
+    }
+
+    public void setPreco_grama(double preco_grama) {
+        this.preco_grama = preco_grama;
+    }
+
+    public double getPeso() {
+        return peso;
+    }
+
+    public void setPeso(double peso) {
+        this.peso = peso;
+    }
+
+    public double getPreco_final() {
+        return preco_final;
+    }
+
+    public void setPreco_final(double preco_final) {
+        this.preco_final = preco_final;
+    }
     
 }
